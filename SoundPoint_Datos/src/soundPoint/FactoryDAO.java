c8be3dcc76a66c77700843f119291377bcddf077
Alan Kelvin López <alankelvin1992@gmail.com>
2014-11-19 22:37:48 -0500

    Revert "Coding doja"
    This reverts commit a4087f3ddbd8265e6985350efd6ad812f1283d14.

--- SoundPoint_Datos/src/soundPoint/FactoryDAO.java
+++
@@ -1,9 +0,0 @@
-package soundPoint;
-
-public class FactoryDAO {
-	private static final String MONGO_URL = "jdbc:mongo://127.0.0.1:8080";
-	private static final String MONGO_DRIVER = "jdbc:mongo://<serverName>/<databaseName>";
-	public static String getMongoDriver() {
-		return MONGO_DRIVER;
-	}
-}
--- SoundPoint_Web/src/view/data/SoundPointView.java
+++
@@ -1,5 +0,0 @@
-package view.data;
-
-public class SoundPointView {
-
-}
